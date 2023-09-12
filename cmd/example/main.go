d033fa0416fc48d80bc85a7f2ad276817d87f88b
oneum20 <woom2290@gmail.com>
2023-09-12 17:02:11 +0900

    Add real-time TTY input/output handling feature.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/oneum20/example/sshstd"
 
 func main() {
-	sshstd.ExecCase01()
-	sshstd.ExecCase02()
+	// sshstd.ExecCase01()
+	// sshstd.ExecCase02()
+	sshstd.ExecCase03()
 }
--- pkg/sshstd/sshstd.go
+++ pkg/sshstd/sshstd.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
+	"sync"
 	"time"
 
 	"golang.org/x/crypto/ssh"
@@ -61,7 +63,9 @@ func (s *SSHBuf) reader() {
 	var data = make([]byte, 1024)
 
 	for {
+		fmt.Println(">> reading...")
 		n, err := s.Stdout.Read(data)
+		fmt.Println(">> n: ", n)
 
 		if err != nil {
 			fmt.Println(err.Error())
@@ -78,12 +82,50 @@ func (s *SSHBuf) getData() {
 	for {
 		select {
 		case d := <-s.Data:
+			fmt.Println(">> Receved Data")
 			s.Buffer += string(d)
 			fmt.Println(s.Buffer)
 		case <-t.C:
-			return
+			fmt.Println(">> Timeout")
+			// return
 		}
 	}
+}
+
+func MuxShell(w io.Writer, r io.Reader) (chan<- string, <-chan string) {
+	in := make(chan string, 1)
+	out := make(chan string, 1)
+	var wg sync.WaitGroup
+	wg.Add(1) //for the shell itself
+	go func() {
+		for cmd := range in {
+			wg.Add(1)
+			w.Write([]byte(cmd + "\n"))
+			wg.Wait()
+		}
+	}()
+	go func() {
+		var (
+			buf [65 * 1024]byte
+			t   int
+		)
+		for {
+			n, err := r.Read(buf[t:])
+			if err != nil {
+				fmt.Println(err)
+				close(in)
+				close(out)
+				return
+			}
+			t += n
+			if buf[t-2] == '$' { //assuming the $PS1 == 'sh-4.3$ '
+				out <- string(buf[:t])
+				t = 0
+				wg.Done()
+			}
+		}
+	}()
+	return in, out
 }
 
 func ExecCase01() {
@@ -136,12 +178,53 @@ func ExecCase02() {
 
 	go s.reader()
 	go s.getData()
+	s.Stdin.Write([]byte("ls \n"))
 
-	s.Stdin.Write([]byte("ls -al\n"))
-
+	s.Stdin.Write([]byte("pwd \n"))
 	session.Wait()
 
-	fmt.Println(s.Stdout)
-	fmt.Println(s.Buffer)
+}
+
+func ExecCase03() {
+	s := &SSHBuf{Data: make(chan []byte, 1024)}
+	config := getConfig()
+	conn := newConn(config)
+	defer func() {
+		fmt.Println("Connection Closed")
+		conn.Close()
+	}()
+
+	session := newSession(conn)
+	defer func() {
+		fmt.Println("Session Closed")
+		session.Close()
+	}()
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		log.Fatal(err)
+	}
+
+	// 입출력 스트림 설정
+	s.Stdin, _ = session.StdinPipe()
+	s.Stdout, _ = session.StdoutPipe()
+
+	// TTY를 통해 원격 서버로부터 데이터 읽기
+	go func() {
+		io.Copy(os.Stdout, s.Stdout)
+	}()
+
+	// 사용자 입력을 TTY로 전송
+	go func() {
+		io.Copy(s.Stdin, os.Stdin)
+	}()
+
+	session.Shell()
+	session.Wait()
 
 }
